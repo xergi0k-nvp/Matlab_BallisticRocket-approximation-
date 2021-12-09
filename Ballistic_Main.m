ce3863503d7ec338c329f8e82380ff0c97323fee
xergi0k-nvp <95773976+xergi0k-nvp@users.noreply.github.com>
2021-12-09 10:59:22 +0300

    adding docs and some tweaks

--- Ballistic_Main.m
+++ Ballistic_Main.m
@@ -8,7 +8,7 @@ for i = 1:size(rocketParams, 2)
     
     % 1 стадия полёта - дозвуковая (или выбранная пользователем пороговая) скорость
     init = [0 0];       % начальные значения для 1 стадии
-    opt1 = odeset('Events', @StopCond1);
+    opt1 = odeset('Events', @(t,y) StopCond1(t, y, i));
     [t, y] = ode45(@(t,y) Ballistic_1Stage(t, y, i), tspan, init, opt1);
     Y = y(:, 1);	% массив координат y
     V = y(:, 2);	% массив скоростей
@@ -19,13 +19,13 @@ for i = 1:size(rocketParams, 2)
     
     h = Y(end, 1);  % высота при которой достигается пороговая скорость
     
-    m0 = CalcMassChange(rocketParams(i).m0, rocketParams(i).mk, rocketParams(i).alfa, rocketParams(i).U, t(end))
+    m0 = CalcMassChange(rocketParams(i).m0, rocketParams(i).mk, rocketParams(i).alfa, rocketParams(i).U, t(end));
 
     % 2 стадия - достижение пороговой скорости, добавляется горизонтальная
     % составляющая скорости
     
     init = [rocketParams(i).Vctrl rocketParams(i).teta 0 h];       % начальные значения для 2 стадии
-    opt2 = odeset('Events', @StopCond2);
+    opt2 = odeset('Events', @(t,y) StopCond2(t, y));
     [t, xy] = ode45(@(t,y) Ballistic_2Stage(t, y, i, m0), tspan, init, opt2);
 
     T = [T; t + T(end)];                    % объединение массивов времени с 2 фаз
@@ -43,10 +43,10 @@ end
 
 %% event-функция для решателя ode45
 % остановка функции при V = контрольная скорость
-function [value, isterminal, direction] = StopCond1(t, y) 
+function [value, isterminal, direction] = StopCond1(t, y, i) 
 global rocketParams
 
-value      = y(2) - rocketParams(1).Vctrl;  % проверяемая величина
+value      = y(2) - rocketParams(i).Vctrl;  % проверяемая величина
 isterminal = 1;     % остановка вычислений
 direction  = 1;     % направление роста
 
